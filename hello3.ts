6abf86d53699ad1595fb13d4c7bed643fee2147a
alimisettyrakesh23 <rakesh.blovespets@supertails.com>
2025-11-08 19:10:00 +0530

    updated

--- hello3.ts
+++ hello3.ts
@@ -1,15 +1,3 @@
 const express=require('express')
 const wow=require('../hello2.js')
 const wow2=require('../react.jsx')
-kjnkjn
-
-hello tsjnjh
-wecjbjrbgjkkn
-wtevgrknkjnjkn
-xwdxkjnjhnjeg
-uhiuhuhhuices
-kjnkjnjknkj
-kjn
-      oiijoijiojkjkn
-      ubjbhjjh
-      nkjnkjnxe
